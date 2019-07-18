01eeebd368f53e4635ab394b0e400b3ddfc65f48
arzonca <arz@agh.edu.pl>
2019-07-18 08:37:00 +0200

    po dopisaniu wczytywaniu employees oraz plik salaried_employees.txt

--- 1707_41_files_1.py
+++ 1707_41_files_1.py
@@ -1,16 +1,29 @@
-from company2 import Task     #importujemy klasę Task z
+from company2 import Task, Salaried     #importujemy klasę Task z    tworzymy obiekty klasy Task
 
 
-def read_tasks_from_file(path):    #czyta plik do listy obiektów
+def read_tasks_from_file(path):    #czyta plik do listy obiektów tzwl otwarcie nowego kontekstu
     tasks = []
-    with open(path) as f:  # otwiera plik i przypcisuje ten plik do zmiennej f
+    with open(path) as f:  # otwiera plik i przypisuje ten plik do zmiennej f
         for line in f:
-            line = line.strip()
-            line = line.split(";")# funkcja strip na obieicie line usuwa biały znak konca linii (line to obiekt napis)
+            line = line.strip()   # wywoła nie linikji z pominieciem enterów (opuszczanie pustych linii), funkcja strip na obieicie line usuwa biały znak konca linii (line to obiekt napis)
+            line = line.split(";")   #dzielimy linie line według ';' i daje zawsze do listy
             description = line[0]
             points = int(line[1])
             tasks.append(Task(description, points))
     return tasks
+
+
+def read_salaried_employees_from_file(path1):
+    employees = []
+    with open(path1) as g:
+        for line in g:
+            line = line.strip()
+            line = line.split(";")
+            name = line[0]
+            age = int(line[1])
+            weekly_salary = int(line[2])
+            employees.append(Salaried(name, age, [], weekly_salary))     #[] przekazywana pusta lista
+    return employees
 
 
 
@@ -20,6 +33,7 @@ if __name__ == '__main__':
     # lines = read_lines_in_file('tasks1.txt')
     # print(lines)
     print(read_tasks_from_file('tasks1.txt'))
+    print(read_salaried_employees_from_file('salaried_employees.txt'))
 
 
 
